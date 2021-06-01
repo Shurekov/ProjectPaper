b73623cc47c29067177d4491c40d62dc4e0b4a5b
avalanche05 <buran05@icloud.com>
2021-06-01 19:19:40 +0300

    Commit

--- app/src/main/java/com/zodiac/user1/MainActivity.java
+++ app/src/main/java/com/zodiac/user1/MainActivity.java
@@ -5,36 +5,40 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 
-import org.xmlpull.v1.XmlPullParser;
-import org.xmlpull.v1.XmlPullParserFactory;
-
-import java.io.IOException;
-import java.io.StringReader;
-import java.util.ArrayList;
-
-
 public class MainActivity extends AppCompatActivity {
-
     private TextView testText;
     private ListView listView;
+    Button chat;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         listView = findViewById(R.id.listView);
+        chat = findViewById(R.id.chatbutton);
         testText = findViewById(R.id.testtext);
         registerReceiver(receiver, new IntentFilter(GisService.CHANNEL));
         final Intent intent = new Intent(this, GisService.class);
         startService(intent);
+        chat.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, ChatActivity.class);
+                startActivity(i);
+            }
+        });
     }
+
 
     @Override
     protected void onDestroy() {
